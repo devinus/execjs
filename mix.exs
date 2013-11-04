08795982bc8f4068421ff33f4d8ec18e2bf6e8d0
Devin Torres <devin@devintorr.es>
2013-11-04 12:30:56 -0600

    Update to Elixir 0.11

--- mix.exs
+++ mix.exs
@@ -6,8 +6,8 @@ defmodule Execjs.Mixfile do
   def project do
     [ app: :execjs,
       version: @version,
-      elixir: "~> 0.10.3",
-      deps: deps(Mix.env) ]
+      elixir: ">= 0.11.0",
+      deps: deps ]
   end
 
   # Configuration for the OTP application
@@ -17,11 +17,7 @@ defmodule Execjs.Mixfile do
 
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
-  defp deps(:dev) do
-    deps(:prod) ++ [{ :benchmark, github: "meh/elixir-benchmark" }]
-  end
-
-  defp deps(_) do
-    [ { :jazz, github: "meh/jazz", tag: "v0.0.1" } ]
+  defp deps do
+    [ { :jazz, github: "meh/jazz" } ]
   end
 end
